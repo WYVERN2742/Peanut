ad097b62707c72c7850cc247b8df5c2a48a9f0e5
Brandagot <17954025+Brandagot@users.noreply.github.com>
2024-12-01 16:36:34 +0000

    Outline finished for the BNO085 and sh2 modules. Created following Adafruit's implementation.

--- src/drivers/BNO085.rs
+++ src/drivers/BNO085.rs
@@ -1,13 +1,43 @@
+use embedded_hal::i2c::{I2c, SevenBitAddress};
+use bl602_hal::i2c::Error;
+use super::sh2::{SensorId, RecordType, Sh2SensorValue};
 
-struct BNO085 {
+pub struct BNO085<'a> {
     address: SevenBitAddress,
-    name: str,
+    name: &'a str,
+    initialized: bool,
 }
 
-impl BNO085 {
-	pub fn new(address: SevenBitAddress, name:&str) -> BNO085 {
-		
+impl<'a> BNO085<'a> {
+	pub fn new(address: SevenBitAddress, name:&'a str) -> BNO085<'a> {
+		Self {address: address, name: name, initialized: false}
 	}
 
+    // Initializes comms with the BNO085 module over I2C.
+    pub fn init(&self) -> Result<(), Error>{
+        Ok(())
+    }
 
+    // Resets the BNO085 hardware.
+    pub fn reset() -> Result<(), Error>{
+        Ok(())
+    }
+
+    /// Enables a report/feature to start reporting
+    /// inputs from the sensor. 
+    pub fn enable_report(sensor_id: SensorId, interval: u32) -> Result<(), Error> {
+        Ok(())
+    }
+
+    /// Disables a report/feature to stop reporting
+    /// inputs from the sensor.
+    pub fn disable_report(sensor_id: SensorId) -> Result<(), Error>{
+        Ok(())
+    }
+
+    /// Fills sensor value with report data, if data is available.
+    /// Result will be false if there is no report available.
+    pub fn get_sensor_event(sensor_value: &mut Sh2SensorValue) -> Result<bool, Error>{
+        Ok(false)
+    }
 }
