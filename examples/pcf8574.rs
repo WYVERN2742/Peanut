fcfb75a2f210ac619cb3aaf45c649bf44dcde281
WYVERN2742 <35181365+WYVERN2742@users.noreply.github.com>
2024-12-01 17:51:15 +0000

    PCF8574 Drivers and example now working on the devkit!

--- examples/pcf8574.rs
+++ examples/pcf8574.rs
@@ -1,12 +1,10 @@
 // Don't include standard rust library, since it's incompatible with embedded systems
 #![no_std]
-
 // We use our own entrypoint marked with #[riscv_rt::entry], not rust's default.
 #![no_main]
 
 // import Hardware Abstraction Layer for bl602
 use bl602_hal as hal;
-use peanut::drivers::PCF8574;
 
 use core::fmt::Write;
 use embedded_hal::delay::DelayNs;
@@ -19,19 +17,18 @@ use hal::{
 	serial::*,
 };
 
-use crate::drivers::MCP4728;
+use peanut::drivers::PCF8574;
 
 // We want our embedded system to halt indefinitely when we panic.
 // We discard the import since its functionality is a side-effect
 use panic_halt as _;
 
 // define a baudrate for serial communication
-const BAUD_RATE:u32 = 1000000;
+const BAUD_RATE: u32 = 115200;
 
 // RiscV entrypoint. The ! notation indicates we have no panic handler
 #[riscv_rt::entry]
 fn main() -> ! {
-
 	// Load all peripherals
 	let dp = pac::Peripherals::take().unwrap();
 	// split the register block into individual pins and modules
@@ -51,7 +48,12 @@ fn main() -> ! {
 	let mux7 = parts.uart_mux7.into_uart0_rx();
 
 	// Configure our UART to our baudrate, and use the pins we configured above
-	let mut serial = Serial::new(dp.UART0, Config::default().baudrate(BAUD_RATE.Bd()), ((pin16, mux0), (pin7, mux7)), clocks);
+	let mut serial = Serial::new(
+		dp.UART0,
+		Config::default().baudrate(BAUD_RATE.Bd()),
+		((pin16, mux0), (pin7, mux7)),
+		clocks,
+	);
 
 	// Pins for LED for Pinecone dev board
 	let mut blue_led = parts.pin11.into_pull_down_output();
@@ -61,15 +63,27 @@ fn main() -> ! {
 	// Create a delay function using the riscv chip cycle counter
 	let mut delay = bl602_hal::delay::McycleDelay::new(clocks.sysclk().0);
 
-	let mut i2c = hal::i2c::I2c::new(
-		dp.I2C,
-		(scl, sda),
-		100_000u32.Hz(),
-		clocks,
-	);
+	let scl = parts.pin4.into_i2c_scl();
+	let sda = parts.pin3.into_i2c_sda();
 
-	let mut pcf = PCF8574::new();
+	let mut i2c = hal::i2c::I2c::new(dp.I2C, (scl, sda), 100_000u32.Hz(), clocks);
+
+	let mut pcf = PCF8574::PCF8574::new(&mut i2c, 0x20, "PCF");
+
+	serial.write_str("PCF INIT\n");
+	pcf.init();
 
 	loop {
+		delay.delay_ms(1000);
+
+		red_led.set_high();
+
+		pcf.set_pin(0, true);
+
+		delay.delay_ms(1000);
+
+		pcf.set_pin(0, false);
+
+		red_led.set_low();
 	}
 }
--- src/drivers/PCF8574.rs
+++ src/drivers/PCF8574.rs
@@ -1,41 +1,83 @@
-use embedded_hal::i2c::{I2c, SevenBitAddress};
-use bl602_hal::i2c::Error;
+use bl602_hal::gpio::I2c as gpio_i2c;
+use bl602_hal::gpio::{Pin4, Pin3};
+use bl602_hal::i2c::{Error, I2c as hal_i2c};
+use bl602_pac::I2C as pac_i2c;
+use embedded_hal::i2c::{I2c as embedded_i2c, SevenBitAddress};
 
-/// 
+/// PCF8574A - Remote 8-Bit I2C IO Expander
+///
+/// This 8-bit IO expander features an 8-bit quasi-bidirectional latched
+/// outputs. Each port can be used as an input or output without the use of a
+/// data-direction signal.
 pub struct PCF8574<'a> {
 	address: SevenBitAddress,
 	name: &'a str,
-	i2c: &'a dyn I2c<Error = Error>,
+	i2c: &'a mut hal_i2c<pac_i2c, (Pin4<gpio_i2c>, Pin3<gpio_i2c>)>,
+	last_set: u8,
 }
 
 impl<'a> PCF8574<'a> {
-	pub fn new(i2c:&dyn I2c<Error = Error>, address: SevenBitAddress, name:&str) -> PCF8574<'a> {
+	pub fn new(
+		i2c: &'a mut hal_i2c<pac_i2c, (Pin4<gpio_i2c>, Pin3<gpio_i2c>)>,
+		address: SevenBitAddress,
+		name: &'a str,
+	) -> PCF8574<'a> {
 		return Self {
 			i2c: i2c,
 			address: address,
 			name: name,
+			last_set: 0,
 		};
 	}
 
-	pub fn set_all_pins(&self, value:u8) -> Result<(), Error> {
-		return self.i2c.write(self.address, &[value])
+	/// Initialize the PCF8574 by setting all pins to LOW
+	pub fn init(&mut self) {
+		self.set_all_pins(0b0000_0000);
 	}
 
-	pub fn set_pin(&self, pin:u8) -> Result<(), Error> {
-		let value:u8 = 1;
-		return self.i2c.write(self.address, &[value << pin])
+	/// Set all IO pins to the corresponding output value interpreted as each bit per pin:
+	/// e.g 0b0000_1010 sets pins P4 and P2 to HIGH, and all other pins to LOW.
+	pub fn set_all_pins(&mut self, value: u8) -> Result<(), Error> {
+		self.last_set = value;
+		return self.i2c.write(self.address, &[value]);
 	}
 
-	pub fn read_all_pins(&self) -> Result<u8, Error> {
-		let mut value:[u8; 1] = [0];
+	/// Set a pin to HIGH or LOW
+	pub fn set_pin(&mut self, pin: u8, high: bool) -> Result<(), Error> {
+		if high {
+			self.last_set = self.last_set | (1 << pin)
+		} else {
+			self.last_set = self.last_set & !(1 << pin)
+		}
+		return self.i2c.write(self.address, &[self.last_set]);
+	}
+
+	/// Read all pins as LOW or HIGH.
+	///
+	/// A pin may be pulled down to LOW or up to HIGH by external sources.
+	///
+	/// Result is a 8-bit value, e.g 0b0010_0010 with P2 and P6 pulled HIGH
+	pub fn read_all_pins(&mut self) -> Result<u8, Error> {
+		let mut value: [u8; 1] = [0];
 		let result = self.i2c.read(self.address, &mut value);
-		return Ok(value[0])
+		if result.is_err() {
+			return Err(result.unwrap_err());
+		} else {
+			return Ok(value[0]);
+		}
 	}
 
-	pub fn read_pin(&self, pin:u8) -> Result<bool, Error> {
-		let mut value:[u8; 1] = [0];
-		let mask:u8 = 1 << pin;
+	/// Read a specific pin as LOW (false) or HIGH (true).
+	///
+	/// A pin may be pulled down to LOW or up to HIGH by external sources.
+	pub fn read_pin(&mut self, pin: u8) -> Result<bool, Error> {
+		let mut value: [u8; 1] = [0];
+		let mask: u8 = 1 << pin;
 		let result = self.i2c.read(self.address, &mut value);
-		return Ok((value[0] & mask) == mask)
+		if result.is_err() {
+			return Err(result.unwrap_err());
+		} else {
+			return Ok((value[0] & mask) == mask);
+		}
 	}
 }
